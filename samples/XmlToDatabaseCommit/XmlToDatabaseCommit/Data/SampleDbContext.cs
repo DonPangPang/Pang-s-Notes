2dd9b35a49389762ed1c92ca51ce97e7bc51b7b0
Powers <powertle@outlook.com>
2024-01-02 17:22:52 +0800

    testproj、efcore

---
+++ samples/XmlToDatabaseCommit/XmlToDatabaseCommit/Data/SampleDbContext.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace XmlToDatabaseCommit.Data;
+
+/// <summary>
+/// 
+/// </summary>
+public class SampleDbContext : DbContext
+{
+    public SampleDbContext(DbContextOptions<SampleDbContext> options) : base(options) { }
+
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        //var xmlCommentHelper = new XmlCommentHelper();
+        //xmlCommentHelper.LoadAll();
+
+        //foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+        //{
+        //    var typeComment = xmlCommentHelper.GetTypeComment(entityType.ClrType);
+        //    modelBuilder.Entity(entityType.ClrType).ToTable(t => t.HasComment(typeComment));
+        //    foreach (var property in entityType.ClrType.GetProperties().Where(x => x.IsPubliclyWritable()))
+        //    {
+        //        var propertyComment = xmlCommentHelper.GetFieldOrPropertyComment(property);
+        //        modelBuilder.Entity(entityType.ClrType).Property(property.Name).HasComment(propertyComment);
+        //    }
+        //}
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
+
+/// <summary>
+/// 用户表
+/// </summary>
+public class User
+{
+    /// <summary>
+    /// Id
+    /// </summary>
+    public Guid Id { get; set; }
+    /// <summary>
+    /// 姓名
+    /// </summary>
+    [Comment("姓名")]
+    public string Name { get; set; } = string.Empty;
+    /// <summary>
+    /// 邮箱
+    /// </summary>
+    public string Email { get; set; } = string.Empty;
+}
---
+++ samples/XmlToDatabaseCommit/XmlToDatabaseCommit/Program.cs
@@ -0,0 +1,43 @@
+using Microsoft.EntityFrameworkCore;
+using XmlToDatabaseCommit.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<SampleDbContext>(opts =>
+{
+    opts.UseSqlite("Data Source=sample.db");
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+await using var scoped = app.Services.CreateAsyncScope();
+using var db = scoped.ServiceProvider.GetRequiredService<SampleDbContext>();
+try
+{
+    db.Database.EnsureDeleted();
+    db.Database.EnsureCreated();
+}
+catch
+{
+    Console.WriteLine("seed error.");
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
