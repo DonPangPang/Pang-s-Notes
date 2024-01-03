f0a6be8f675ea2ae57f524b61b04c054cd12f501
Powers <powertle@outlook.com>
2024-01-03 09:03:07 +0800

    ef

--- samples/XmlToDatabaseCommit/XmlToDatabaseCommit/Data/SampleDbContext.cs
+++ samples/XmlToDatabaseCommit/XmlToDatabaseCommit/Data/SampleDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace XmlToDatabaseCommit.Data;
 
@@ -13,19 +14,20 @@ public class SampleDbContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        //var xmlCommentHelper = new XmlCommentHelper();
-        //xmlCommentHelper.LoadAll();
 
-        //foreach (var entityType in modelBuilder.Model.GetEntityTypes())
-        //{
-        //    var typeComment = xmlCommentHelper.GetTypeComment(entityType.ClrType);
-        //    modelBuilder.Entity(entityType.ClrType).ToTable(t => t.HasComment(typeComment));
-        //    foreach (var property in entityType.ClrType.GetProperties().Where(x => x.IsPubliclyWritable()))
-        //    {
-        //        var propertyComment = xmlCommentHelper.GetFieldOrPropertyComment(property);
-        //        modelBuilder.Entity(entityType.ClrType).Property(property.Name).HasComment(propertyComment);
-        //    }
-        //}
+        var xmlCommentHelper = new XmlCommentHelper();
+        xmlCommentHelper.LoadAll();
+
+        foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+        {
+            var typeComment = xmlCommentHelper.GetTypeComment(entityType.ClrType);
+            modelBuilder.Entity(entityType.ClrType).ToTable(t => t.HasComment(typeComment));
+            foreach (var property in entityType.ClrType.GetProperties().Where(x => x.IsPubliclyWritable()))
+            {
+                var propertyComment = xmlCommentHelper.GetFieldOrPropertyComment(property);
+                modelBuilder.Entity(entityType.ClrType).Property(property.Name).HasComment(propertyComment);
+            }
+        }
 
         base.OnModelCreating(modelBuilder);
     }
@@ -43,10 +45,9 @@ public class User
     /// <summary>
     /// 姓名
     /// </summary>
-    [Comment("姓名")]
-    public string Name { get; set; } = string.Empty;
+    public required string Name { get; set; }
     /// <summary>
     /// 邮箱
     /// </summary>
-    public string Email { get; set; } = string.Empty;
+    public required string Email { get; set; }
 }
